5c458ee7d6ee0044152657a6f38666adc119d101
ester shmuel <eshmuel@g.jct.ac.il>
2021-10-10 18:14:40 +0300

    Initial code+Welcome functions

---
+++ T.Targil0/Program8468.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T.Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8468();
+            welcome8727();
+            Console.ReadKey();
+
+        }
+        static partial void welcome8727();
+        private static void Welcome8468()
+        {
+            Console.WriteLine("Enter your name: ");
+            String name_user = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name_user);
+        }
+    }
+}
