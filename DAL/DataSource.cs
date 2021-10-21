1aca5f9693aeff762473a39240b7b71beb0628f9
orly1597 <78793998+orly1597@users.noreply.github.com>
2021-10-21 18:04:04 +0300

    21.10.2021

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -45,8 +45,49 @@ namespace DalObject
                 };
             }
 
+            for (int i = 0; i < 2; i++)
+            {
+                stations[Config.index_stations++] = new Station()
+                {
+                    Id = rand.Next(10000,99999),
+                    Name=i,
+                    Longitude=rand.Next(1000,4000),
+                    Lattitude=rand.Next(1000,4000),
+                    ChargeSlots =rand.Next(1,10), 
+                };
+            }
 
+            for (int i = 0; i < 10; i++)
+            {
+                string newName="";
+                for (int j = 0; j < 5; j++)
+                {
+                    int temp = rand.Next(97, 122);
+                    newName += (char)temp;
+                }
+                customers[Config.index_customers++] = new Customer()
+                {
+                    Id = rand.Next(100000000, 999999999),
+                    Name = newName,
+                    Phone = "05" + rand.Next(0, 4) + "-" + rand.Next(10000000, 99999999),
+                    Longitude=rand.Next(-180, 180),
+                    Lattitude=rand.Next(-90,90),
+                };
+            }
 
+            for (int i = 0; i < 10; i++)
+            {
+                parcels[Config.index_parcels++] = new Parcel()
+                {
+                    Id=rand.Next(100000,999999),
+                    Senderld=rand.Next(100000000,999999999),
+                    Targetld=rand.Next(100000000,999999999),
+                    Weight=(WeightCategories)rand.Next(0,2),
+                    Priority=(Priorities)rand.Next(0,2),
+                    Droneld = rand.Next(1000, 9999),
+                    Requested= new DateTime(rand.Next(2021,2023),rand.Next(1,12),rand.Next(1,31)),
+                };
+            }
 
         }
 
