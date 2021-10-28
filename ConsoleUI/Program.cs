b502871789ae27513886507b3da57452e1dea39a
orly1597 <78793998+orly1597@users.noreply.github.com>
2021-10-28 17:24:57 +0300

    28.10.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using IDAL.DO;
+using DAL;
+using DalObject;
 
 namespace ConsoleUI
 {
-
+    enum programDeli { addingOptions, }
+    enum Add { addCustomer, addDrone, addStation, addParcel }; 
+    
+   
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("To adding customer, drone, station or parcel - enter 0\n");
+                Console.WriteLine("To update items");
+
+
+
+
+                int n = int.Parse(Console.ReadLine());
+                switch (n)
+                {
+                    case program.addingOptions:
+                     
+                        Console.WriteLine("To add customer - enter 0\nTo add drone - enter 1\nTo add station - enter 2\nTo add parcel - enter 3\n");
+                        int choiceAdd = int.Parse(Console.ReadLine());
+                        switch (choiceAdd)
+                        {
+                            case Add.addCustomer:
+                                DalObject.DalObject a = new DalObject.DalObject();
+                                a.addCustomer();
+                                break;
+                            case Add.addDrone:
+                                break;
+                            case Add.addStation:
+                                break;
+                            case Add.addParcel:
+                                break;
+                            default:
+                                Console.WriteLine("Please enter again:");
+                                break;
+                        }
+                }
+
+            }
+
+
+
+        }
     }
+
+
+
+}
 }
--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -7,101 +7,249 @@ using IDAL.DO;
 
 namespace DalObject
 {
-   public class DalObject//מחלקה שבתוכה כל המתודות שיעסקו בהבאת נתונים
+    public class DalObject//מחלקה שבתוכה כל המתודות שיעסקו בהבאת נתונים
     {
         internal static Random rand = new Random();
 
-        DalObject() { DataSource.Initialize(); }
-        public void addStaion() 
+        public DalObject()
+        { 
+            DataSource.Initialize();
+        }
+
+        public void addStaion()
         {
+            Console.WriteLine("Please enter station's id:");
+            int id=int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter station's name:");
+            int name= int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the station's longitude");
+            double longitude = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the station's lattitude");
+            double lattitude= double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the station's chargeSlots");
+            int chargeSlots= int.Parse(Console.ReadLine());
+
             DataSource.stations[DataSource.Config.index_stations++] = new Station()
             {
-                Id = rand.Next(10000, 99999),
-                Name = rand.Next(1,1000),
-                Longitude = rand.Next(1000, 4000),
-                Lattitude = rand.Next(1000, 4000),
-                ChargeSlots = rand.Next(1, 10),
-            }; 
+                Id = id,
+                Name = name,
+                Longitude = longitude,
+                Lattitude = lattitude,
+                ChargeSlots = chargeSlots,
+            };
 
         }
 
-        //public void addDrone()
-        //{
-        //    DataSource.drones[DataSource.Config.index_drones++] = new Drone()
-        //    {
-        //        Id = rand.Next(1000, 9999),
-        //        Model = "5G",
-        //        MaxWeight = (WeightCategories)rand.Next(0, 2),
-        //        Status = (DroneStatuses)rand.Next(0, 2),
-        //        Battery = rand.Next(0, 100)
-        //    };
-        //}
-        public void addDrone()//דוגמא שנעתיק לכול הפונקציות
+        public void addDrone()//To add drone
         {
+            Console.WriteLine("Please enter drone's id:");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter drone's model:");
+            string model = Console.ReadLine();
+            Console.WriteLine("Please enter drone's weight categories - 0 for light, 1 for medium, 2 for heavy:");
+            int maxWeight = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter drone's status - 0 for available, 1 for maintenance, 2 for delivery:");
+            int status = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter drone's battery:");
+            int battery = int.Parse(Console.ReadLine());
+
             DataSource.drones[DataSource.Config.index_drones++] = new Drone()
-            {                                         ///הוספת הסבר מה מוסיפים///
-                Id =int.Parse(Console.ReadLine()),
-                Model = Console.ReadLine(),
-                MaxWeight = (WeightCategories)int.Parse(Console.ReadLine()),
-                Status = (DroneStatuses)int.Parse(Console.ReadLine()),
-                Battery = int.Parse(Console.ReadLine())
+            {
+                Id = id,
+                Model = model,
+                MaxWeight = (WeightCategories)maxWeight,
+                Status = (DroneStatuses)status,
+                Battery = battery
             };
         }
 
-        public void addCustomer()
+        public void addCustomer()//To add customer
         {
-            string newName = "";
-            for (int j = 0; j < 5; j++)
-            {
-                int temp = rand.Next(97, 122);
-                newName += (char)temp;
-            }
+            Console.WriteLine("Please enter your id:");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your phone:");
+            string phone = Console.ReadLine();
+            Console.WriteLine("Please enter lattitude:");
+            double lattitude = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter longitude:");
+            double longitude = double.Parse(Console.ReadLine());
+
+
             DataSource.customers[DataSource.Config.index_customers++] = new Customer()
             {
-                Id = rand.Next(100000000, 999999999),
-                Name = newName,
-                Phone = "05" + rand.Next(0, 4) + "-" + rand.Next(10000000, 99999999),
-                Longitude = rand.Next(-180, 180),
-                Lattitude = rand.Next(-90, 90),
+                Id = id,
+                Name = name,
+                Phone = phone,
+                Lattitude = lattitude,
+                Longitude = longitude
             };
         }
 
-        public void addParcel(int Id, int Senderld, int Targetld, WeightCategories Weight, Priorities Priority, DateTime Requested, int Droneld, DateTime Scheduled, DateTime PickedUp, DateTime Delivered);
-        public void addParcel(int Id, int Senderld, int Targetld, WeightCategories Weight, Priorities Priority, DateTime Requested, int Droneld, DateTime Scheduled, DateTime PickedUp, DateTime Delivered);
+        public void addParcel()
         {
+            Console.WriteLine("Please enter parcel's id:");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter sender's id:");
+            int senderld = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter target's id:");
+            int targetld = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter drone's weight categories - 0 for light, 1 for medium, 2 for heavy:");
+            int maxWeight = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the delivery's priority - 0 for normal, 1 for fast, 2 for emergency:");
+            int priority = int.Parse(Console.ReadLine());
+
             DataSource.parcels[DataSource.Config.index_parcels++] = new Parcel()
             {
-                Id = rand.Next(100000, 999999),
-                Senderld = rand.Next(100000000, 999999999),
-                Targetld = rand.Next(100000000, 999999999),
-                Weight = (WeightCategories)rand.Next(0, 2),
-                Priority = (Priorities)rand.Next(0, 2),
-                Droneld = rand.Next(1000, 9999),
-                Requested = new DateTime(rand.Next(2021, 2023), rand.Next(1, 12), rand.Next(1, 31)),
+                Id = id,
+                Senderld = senderld,
+                Targetld = targetld,
+                Weight = (WeightCategories)maxWeight,
+                Priority = (Priorities)priority,
+                Droneld = 0,
+                Requested = DateTime.Now,
             };
         }
 
-        public void assign_parcel_drone()
+        public void assign_parcel_drone(ref Parcel p, ref Drone d)//assign parcel to drone
         {
             p.Droneld = d.Id;
+            p.Scheduled = DateTime.Now;
         }
 
 
-        public void drone_pick_package_(Drone d)//איסוף חבילה ע"י רחפן
-        { }
-    }
-
-        public void update_parcel_drone (string station, ref Parcel p, ref Drone d, ref Customer c)
+        public void drone_pick_parcel(ref Parcel p, ref Drone d)//איסוף חבילה ע"י רחפן
         {
-            p.Droneld = d.Id;
-            d.Status= DroneStatuses.delivery;
-            p.Targetld = c.Id;
+            d.Status = DroneStatuses.delivery;
+            p.PickedUp = DateTime.Now;
+        }
+        public void delivery_arrive_toCustomer(ref Parcel p, ref Drone d)//The delivery arrived to the customer
+        {
+            d.Status = DroneStatuses.available;
+            p.Delivered = DateTime.Now;
+        }
+
+        public void chargingDrone(ref Drone d, ref Station s, ref DroneCharge dc)
+        {
+            dc = new DroneCharge()
+            {
+                Droneld = d.Id,
+                Stationld = s.Id
+            };
             d.Status = DroneStatuses.maintenance;
-            DroneCharge drone_charge = new DroneCharge();
-            drone_charge.Droneld = d.Id;
-            DroneCharge.
+            s.ChargeSlots--;
         }
 
-        public void 
+        public void freeDroneCharge(ref Drone d, ref Station s, ref DroneCharge dc)
+        {
+            dc.Droneld = 0;
+            dc.Stationld = 0;
+            s.ChargeSlots++;
+            d.Status = DroneStatuses.available;
+            d.Battery = 100;
+        }
+
+        public void printStation(int id)
+        {
+            for (int i = 0; i < DataSource.stations.Length; i++)
+            {
+                if (DataSource.stations[i].Id == id)
+                {
+                    DataSource.stations[i].ToString();
+                    break;
+                }
+            }
+        }
+
+        public void printDrone(int id)
+        {
+            for (int i = 0; i < DataSource.drones.Length; i++)
+            {
+                if (DataSource.drones[i].Id == id)
+                {
+                    DataSource.drones[i].ToString();
+                    break;
+                }
+            }
+        }
+
+        public void printCustomer(int id)
+        {
+            for (int i = 0; i < DataSource.customers.Length; i++)
+            {
+                if (DataSource.customers[i].Id == id)
+                {
+                    DataSource.customers[i].ToString();
+                    break;
+                }
+            }
+        }
+
+        public void printParcel(int id)
+        {
+            for (int i = 0; i < DataSource.parcels.Length; i++)
+            {
+                if (DataSource.parcels[i].Id == id)
+                {
+                    DataSource.parcels[i].ToString();
+                    break;
+                }
+            }
+        }
+
+        public void printAllStations(int id)
+        {
+            foreach (Station item in DataSource.stations)
+            {
+                item.ToString();
+            }
+            /*
+            for (int i = 0; i < DataSource.stations.Length; i++)
+            {
+                DataSource.stations[i].ToString();
+
+            }*/
+        }
+
+        public void printAllDrones(int id)
+        {
+            foreach (Drone item in DataSource.drones)
+            {
+                item.ToString();
+            }
+
+            /*for (int i = 0; i < DataSource.drones.Length; i++)
+            {
+                DataSource.drones[i].ToString();
+            }*/
+        }
+
+        public void printAllCustomers(int id)
+        {
+            foreach (Customer item in DataSource.customers)
+            {
+                item.ToString(); 
+            }
+
+            /*for (int i = 0; i < DataSource.customers.Length; i++)
+            {
+                DataSource.customers[i].ToString();
+            }*/
+        }
+
+        public void printAllParcels(int id)
+        {
+            foreach (Parcel item in DataSource.parcels)
+            {
+                item.ToString();
+            }
+            /*
+            for (int i = 0; i < DataSource.parcels.Length; i++)
+            {
+                DataSource.parcels[i].ToString();
+            }*/
+        }
     }
 }
+
