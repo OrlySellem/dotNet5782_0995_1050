b9aa8c71fe6a79fec337515f025d918034f1ade2
orly1597 <78793998+orly1597@users.noreply.github.com>
2021-12-09 19:04:10 +0200

    09.12.2021

--- Bl/MethodGeneral.cs
+++ Bl/MethodGeneral.cs
@@ -110,9 +110,9 @@ namespace IBL
                 heavyWeight = power[3];
                 Drone_charging_speed = power[4];
 
-                IEnumerable<IDAL.DO.Drone> dronesFromDS = dal.getAllDrones();
-                IEnumerable<IDAL.DO.Parcel> parcelsFromDS = dal.getAllParcels();
-                IEnumerable<IDAL.DO.Station> stationFromDS = dal.getAllStation();
+                List<IDAL.DO.Drone> dronesFromDS = (List<IDAL.DO.Drone>)dal.getAllDrones();
+                List<IDAL.DO.Parcel> parcelsFromDS = (List<IDAL.DO.Parcel>)dal.getAllParcels();
+                List<IDAL.DO.Station> stationFromDS = (List<IDAL.DO.Station>)dal.getAllStation();
 
                 foreach (IDAL.DO.Drone itemDrone in dronesFromDS)//pass over the drones's list 
                 {
@@ -184,6 +184,7 @@ namespace IBL
                             if (temp.Status == DroneStatuses.maintenance)
                             {
                                 int indexStation = rand.Next(stationFromDS.Count());
+
                                 temp.CurrentLocation = new Location()
                                 {
                                     Lattitude = stationFromDS[indexStation].Lattitude,
@@ -220,12 +221,14 @@ namespace IBL
                     #endregion
                 }
             }
-            catch (Exception ex)
+            catch (IDAL.DO.DoesntExistException ex)
             {
-
-                throw;
+                throw new GetDetailsProblemException("The customer doesn't exist", ex);          
             }
-
+            catch (IDAL.DO.AlreadyExistException ex)
+            {
+                throw new AddingProblemException("The drone already exist", ex);
+            }
 
 
         }
--- Bl/MethodStation.cs
+++ Bl/MethodStation.cs
@@ -166,6 +166,8 @@ namespace IBL
 
     }
 
+}
+
 
 
 
