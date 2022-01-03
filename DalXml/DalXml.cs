d4e95a1740c962ee93c4ffa7aa8e15e30df77681
ester shmuel <eshmuel@g.jct.ac.il>
2022-01-03 17:16:25 +0200

    welcome back

--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -18,51 +18,134 @@ namespace Dal
 
         #endregion singelton
 
+        string customerPath = @"CustomerXml.xml";//XElement
+        string dronePath = @"DroneXml.xml";  //XElement
+        string droneChargePath = @"DroneChargeXml.xml";//XElement
+        string parcelPath = @"ParcelXml.xml";//XMLSerializer
+        string stationPath = @"Station.xml";//XMLSerializer
+
+
+        #region ADD
         public void addStaion(Station stationToAdd)
         {
-            throw new NotImplementedException();
+
+
+            var listStation = XMLTools.LoadListFromXMLSerializer<Station>(stationPath);
+            if (listStation.Exists(x => x.Id == stationToAdd.Id))
+                throw new AlreadyExistException("station");
+
+            listStation.Add(stationToAdd);
+            XMLTools.SaveListToXMLSerializer(listStation, stationPath);
+
+            // throw new NotImplementedException();
         }
 
         public void addDrone(Drone DroneToAdd)
         {
-            throw new NotImplementedException();
+
+            var listDrone = XMLTools.LoadListFromXMLSerializer<Drone>(dronePath);
+            if (listDrone.Exists(x => x.Id == DroneToAdd.Id))
+                throw new AlreadyExistException("drone");
+
+            listDrone.Add(DroneToAdd);
+            XMLTools.SaveListToXMLSerializer(listDrone, dronePath);
+
+           // throw new NotImplementedException();
         }
 
         public void addCustomer(Customer CustomerToAdd)
         {
-            throw new NotImplementedException();
+            var listCustomer = XMLTools.LoadListFromXMLSerializer<Customer>(customerPath);
+            if (listCustomer.Exists(x => x.Id == CustomerToAdd.Id))
+                throw new AlreadyExistException("customer");
+
+            listCustomer.Add(CustomerToAdd);
+            XMLTools.SaveListToXMLSerializer(listCustomer, customerPath);
+
+            // throw new NotImplementedException();
         }
 
         public void addParcel(Parcel ParcelToAdd)
         {
-            throw new NotImplementedException();
+            var listParcel = XMLTools.LoadListFromXMLSerializer<Parcel>(parcelPath);
+            if (listParcel.Exists(x => x.Id == ParcelToAdd.Id))
+                throw new AlreadyExistException("parcel");
+
+            listParcel.Add(ParcelToAdd);
+            XMLTools.SaveListToXMLSerializer(listParcel, parcelPath);
+
+            // throw new NotImplementedException();
         }
 
+        #endregion ADD
+
+        #region GET 
         public Parcel getParcel(int id)
         {
-            throw new NotImplementedException();
+            var listParcel = XMLTools.LoadListFromXMLSerializer<Parcel>(parcelPath);
+            Parcel parcel = (from findparcel in listParcel
+                             where findparcel.Id == id
+                             select findparcel).FirstOrDefault();
+            if (parcel.Equals(default(Parcel)))
+                throw new DoesntExistentObjectException("parcel");
+            return parcel;
+
+            //  throw new NotImplementedException();
         }
 
         public Drone getDrone(int id)
         {
-            throw new NotImplementedException();
+            var listDrone = XMLTools.LoadListFromXMLSerializer<Drone>(dronePath);
+            Drone drone = (from findDrone in listDrone
+                             where findDrone.Id == id
+                             select findDrone).FirstOrDefault();
+            if (drone.Equals(default(Parcel)))
+                throw new DoesntExistentObjectException("drone");
+            return drone;
+
+            //  throw new NotImplementedException();
         }
 
         public Station getStation(int id)
         {
-            throw new NotImplementedException();
+            var listStation = XMLTools.LoadListFromXMLSerializer<Station>(stationPath);
+            Station station = (from findStation in listStation
+                             where findStation.Id == id
+                             select findStation).FirstOrDefault();
+            if (station.Equals(default(Parcel)))
+                throw new DoesntExistentObjectException("station");
+            return station;
+
+            //  throw new NotImplementedException();
         }
 
         public Customer getCustomer(int id)
         {
-            throw new NotImplementedException();
+            var listCustomer = XMLTools.LoadListFromXMLSerializer<Customer>(customerPath);
+            Customer customer = (from findCustomer in listCustomer
+                             where findCustomer.Id == id
+                             select findCustomer).FirstOrDefault();
+            if (customer.Equals(default(Parcel)))
+                throw new DoesntExistentObjectException("customer");
+            return customer;
+
+            //  throw new NotImplementedException();
         }
 
         public DroneCharge getDroneCharge(int id)
         {
-            throw new NotImplementedException();
+            var listDroneCharge = XMLTools.LoadListFromXMLSerializer<DroneCharge>(parcelPath);
+            DroneCharge droneCharge = (from findDroneCharge in listDroneCharge
+                             where findDroneCharge.Droneld == id
+                             select findDroneCharge).FirstOrDefault();
+            if (droneCharge.Equals(default(DroneCharge)))
+                throw new chargingException("the drone is not charging");
+            return droneCharge;
+
+            //  throw new NotImplementedException();
         }
 
+        #endregion GET 
         public void delFromDrones(Drone droneToDel)
         {
             throw new NotImplementedException();
