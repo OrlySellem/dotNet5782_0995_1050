47e9ced8d3d58af1c37c49c4b44d0d5bfa50e124
orly1597 <78793998+orly1597@users.noreply.github.com>
2022-01-16 23:49:32 +0200

    16.01.2022 23:49

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -24,9 +24,15 @@ namespace PL
     {
         
         public BlApi.IBL mainBl =  BlApi.BlFactory.GetBl();
-      
 
-        //Button insertToDroneList;
+        //על מנת שנוכל להזיז את החלון
+        private void moveWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        } 
+
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -55,52 +61,9 @@ namespace PL
             
         }
 
-        private void sign_Click(object sender, RoutedEventArgs e)
-        {
-           // tableLists.Content = new CustomerWindow(mainBl).ShowDialog();
-        }
-
-        private void LogIn_Click(object sender, RoutedEventArgs e)
-        {
-
-            try
-            {
-                CustomerToList c = (from customer in mainBl.getAllCustomers()
-                                    where IDTextBox.Text == customer.Id.ToString() && passwordUserName.Password == customer.Password
-                                    select customer).FirstOrDefault();
-
-                passwordUserName.Visibility = Visibility.Hidden;
-                ID.Visibility = Visibility.Hidden;
-                Password.Visibility = Visibility.Hidden;
-                IDTextBox.Visibility = Visibility.Hidden;
-                signUp.Visibility = Visibility.Hidden;
-                LogIn.Visibility = Visibility.Hidden;
-                MainGrid.Visibility = Visibility.Visible;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void DronesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void BaseStationsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
     }
 }
